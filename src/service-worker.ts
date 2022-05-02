adf4467326b0f6456335a65e383028c55f2577d0
envl <yyaomingm@gmail.com>
2022-05-03 00:23:17 +0800

    fix: typo & sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,14 +1,12 @@
 /// <reference lib="webworker" />
 
-const worker = self as unknown as ServiceWorkerGlobalScope;
+const worker = self as unknown as ServiceWorkerGlobalScope
 
 worker.addEventListener('fetch', (e) => {
-	e.respondWith(
-		(async function () {
-			const response = await caches.match(e.request);
-			return response || fetch(e.request);
-		})()
-	);
-});
-
-export {};
+  e.respondWith(
+    (async function () {
+      const response = await caches.match(e.request)
+      return response || fetch(e.request)
+    })(),
+  )
+})
